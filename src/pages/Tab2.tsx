478582f365304ff9f85d87d7915156e2a169104b
Charles Darwin <carlos.campos@uisek.edu.ec>
2026-06-17 09:47:56 -0500

    Método POST

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,52 +1,126 @@
-import {  IonButton, IonContent,IonHeader, IonIcon, IonInput, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonIcon, useIonViewWillEnter, IonContent, IonHeader, IonInput, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab2.css';
+import React from 'react';
+import { RepositoryPayload } from '../interfaces/payload';
+import { createRepository } from '../services/GithubServces';
+import { useHistory } from 'react-router';
+import LoadingSpinner from '../components/LoadingSpinner';
+import { fetchRepositories } from '../services/GithubServces';
+import { Repository } from '../interfaces/Repository';
+import { logoGithub, refresh } from 'ionicons/icons';
 
 const Tab2: React.FC = () => {
-  return (
+  const history = useHistory();
 
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [repos,setRepos] = React.useState<Repository[]>([]);
+  
+
+  const repoFormData: RepositoryPayload = {
+    name: '',
+    description: '',
+  };
+
+  const setRepoName = (value: string) => {
+    repoFormData.name = value;
+  };
+
+  const setRepoDescription = (value: string) => {
+    repoFormData.description = value;
+  };
+
+  const saveRepository = () => {
+    if (repoFormData.name.trim() === '') {
+      alert('Este campo es obligatorio');
+      return;
+    };
+
+    setLoading(true);
+
+    createRepository(repoFormData)
+      .then(() => {
+        history.push('/tab1');
+      })
+      .catch(() => {
+        alert('Error al crear repositorio');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    };
+
+      const loadRepos = async() => { 
+  
+      setLoading(true);
+      const reposData = await fetchRepositories();
+      setRepos(reposData);
+      setLoading(false);
+  
+    }
+      useIonViewWillEnter(() =>{
+        loadRepos();
+  
+      });
+  return (
     <IonPage>
       <IonHeader>
         <IonToolbar color="success">
-          <IonTitle> Crear Repositorio</IonTitle>
+          <IonTitle>Agregar repositorio</IonTitle>
         </IonToolbar>
+        <IonButton onClick={loadRepos}>
+          <IonIcon icon={refresh} />
+          </IonButton>
       </IonHeader>
 
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Formulario de repositorio</IonTitle>
+            <IonTitle size="large">Agregar repositorio</IonTitle>
           </IonToolbar>
-
         </IonHeader>
-        <div className='form-container'>
+
+        <div className="form-container">
+
+          <div className="form-header">
+            <h2>Nuevo Repositorio</h2>
+            <p>Complete los datos para crear un repositorio</p>
+          </div>
+
           <IonInput
             className="form-field"
-            label="Nombre del Repositorio"
+            label="Nombre"
             labelPlacement="floating"
             fill="outline"
             placeholder="nombre-repositorio"
+            value={repoFormData.name}
+            onIonChange={(e) => setRepoName(e.detail.value!)}
           />
-          
+
           <IonTextarea
             className="form-field"
             label="Descripción"
             labelPlacement="floating"
             fill="outline"
-            placeholder="Describe tu repositorio"
+            placeholder="descripcion-repositorio"
             rows={6}
-          ></IonTextarea>
+            value={repoFormData.description}
+            onIonChange={(e) => setRepoDescription(e.detail.value!)}
+            autoGrow
+          />
 
           <IonButton
+            className="form-field save-button"
             expand="block"
-            color="success"
-            className="save-button"
+            fill="solid"
+            onClick={saveRepository}
           >
-            Guardar Repositorio
+            Crear repositorio
           </IonButton>
+
         </div>
 
+        {loading && <LoadingSpinner isOpen={loading} />}
       </IonContent>
-     
     </IonPage>
   );
 };
--- src/services/GithubServces.ts
+++ src/services/GithubServces.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { Repository } from '../interfaces/Repository';
+import { RepositoryPayload } from '../interfaces/payload';
 
 const GITHUB_API_BASE_URL = import.meta.env.VITE_GITHUB_API_URL || 'https://api.github.com';
 const GITHUB_API_TOKEN = import.meta.env.VITE_GITHUB_API_TOKEN;
@@ -25,3 +26,25 @@ export const fetchRepositories = async (): Promise<Repository[]> => {
         return [];
     }
 }
+export const createRepository = async (
+    repository: RepositoryPayload
+): Promise<Repository | null> => {
+    try {
+        const response = await axios.post(
+            `${GITHUB_API_BASE_URL}/user/repos`,
+            repository,
+            {
+                headers: {
+                    Authorization: `token ${GITHUB_API_TOKEN}`,
+                },
+            }
+        );
+
+        console.log('Repository created successfully:', response.data);
+
+        return response.data as Repository;
+    } catch (error) {
+        console.error('Error al crear repositorio:', error);
+        return null;
+    }
+};
